a1c579fc23568de11978f1fa74502c2abd9d9814
npa-axelor <92723622+npa-axelor@users.noreply.github.com>
2024-06-14 18:05:16 +0530

    worked on pagination

--- ui/components/pagination/pagination.tsx
+++ ui/components/pagination/pagination.tsx
@@ -1,6 +1,6 @@
-import { Button } from "@ui/components/button"
-import { LuChevronLeft } from "react-icons/lu";
-import { LuChevronRight } from "react-icons/lu";
+import {Button} from '@ui/components/button';
+import {LuChevronLeft} from 'react-icons/lu';
+import {LuChevronRight} from 'react-icons/lu';
 
 type PaginationProps = {
   page?: string | number;
@@ -12,7 +12,14 @@ type PaginationProps = {
   onNext?: any;
 };
 
-function RoundButton({ children, onClick, disabled, active ,className="", ...rest }: any) {
+function RoundButton({
+  children,
+  onClick,
+  disabled,
+  active,
+  className = '',
+  ...rest
+}: any) {
   return (
     <Button
       variant="outline"
@@ -20,8 +27,7 @@ function RoundButton({ children, onClick, disabled, active ,className="", ...res
       onClick={onClick}
       disabled={disabled}
       {...rest}
-      className={`${active ? 'text-primary bg-success-light' : 'text-primary-forground bg-success-dark'} mr-1 ${className}`}
-    >
+      className={`${active ? 'text-primary bg-success-light' : 'text-primary-forground bg-success-dark'} mr-1 ${className}`}>
       {children}
     </Button>
   );
@@ -36,30 +42,36 @@ export function Pagination({
   onNext,
   onPage,
 }: PaginationProps) {
+  pages = pages === 0 ? 1 : pages;
+
   return (
     <div className="flex items-center gap-1 flex-wrap">
-      <RoundButton onClick={onPrev} disabled={disablePrev} className="p-2 bg-transparent border-0">
+      <RoundButton
+        onClick={onPrev}
+        disabled={disablePrev}
+        className="p-2 bg-transparent border-0">
         <div className="flex">
           <LuChevronLeft className="h-4 w-4 text-base" />
         </div>
       </RoundButton>
-      {Array.from({ length: Number(pages) }, (_, i) => {
+      {Array.from({length: Number(pages)}, (_, i) => {
         const current = i + 1;
-
         return (
           <RoundButton
             key={i}
             onClick={() => onPage?.(current)}
-            outline={Number(page!== current)}
+            outline={String(page !== current)}
             className="p-4 py-2 pointer-events-none border-0 rounded-full !opacity-100"
-            disabled={true}
-            active
-          >
+            active={Number(page) === current}
+            >
             {current}
           </RoundButton>
         );
       })}
-      <RoundButton onClick={onNext} disabled={disableNext} className="p-2 bg-transparent border-0">
+      <RoundButton
+        onClick={onNext}
+        disabled={disableNext}
+        className="p-2 bg-transparent border-0">
         <div className="flex">
           <LuChevronRight className="h-4 w-4 text-base " />
         </div>
