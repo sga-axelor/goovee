b14e5f81f1c0a0de904f365d592a2eddfdcbba71
npa-axelor <92723622+npa-axelor@users.noreply.github.com>
2024-06-15 11:21:42 +0530

    shade cn module changes

--- ui/components/badge.tsx
+++ ui/components/badge.tsx
@@ -29,8 +29,10 @@ export interface BadgeProps
   extends React.HTMLAttributes<HTMLDivElement>,
     VariantProps<typeof badgeVariants> {}
 
-function Badge({className, variant, ...props}: BadgeProps) {
-  return <div className={cn(badgeVariants({variant}), className)} {...props} />;
+function Badge({className, variant, children, ...props}: BadgeProps) {
+  return (
+    <span className={cn(badgeVariants({variant}), className)} {...props} />
+  );
 }
 
 export {Badge, badgeVariants};
--- ui/components/logo/logo.tsx
+++ ui/components/logo/logo.tsx
@@ -1,11 +1,13 @@
 import Image from 'next/image';
 
-export function Logo({height = 50, ...rest}: any) {
+export function Logo({height = 50, width = 100, ...rest}: any) {
   return (
     <Image
       alt="Company Logo"
       src="/images/logo.png"
       height={height}
+      width={width}
+      style={{width: 'auto', height: 'auto'}}
       {...rest}
     />
   );
