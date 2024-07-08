24173aba64e68d0375e9158acd76bc9149757aa5
npa-axelor <92723622+npa-axelor@users.noreply.github.com>
2024-07-08 19:51:09 +0530

    Fix lint

--- app/[tenant]/[workspace]/(subapps)/shop/common/ui/components/product-list/product-list.tsx
+++ app/[tenant]/[workspace]/(subapps)/shop/common/ui/components/product-list/product-list.tsx
@@ -10,7 +10,7 @@ import {MdGridView} from 'react-icons/md';
 import {MdOutlineList} from 'react-icons/md';
 
 // ---- CORE IMPORTS ---- //
-import {Pagination,TextField} from '@/ui/components';
+import {Pagination, TextField} from '@/ui/components';
 import {useCart} from '@/app/[tenant]/[workspace]/cart-context';
 import {useWorkspace} from '@/app/[tenant]/[workspace]/workspace-context';
 import {i18n} from '@/lib/i18n';
