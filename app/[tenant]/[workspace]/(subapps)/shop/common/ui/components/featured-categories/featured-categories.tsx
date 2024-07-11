12db4e2deff52834f665d05859800aef6108c548
npa-axelor <92723622+npa-axelor@users.noreply.github.com>
2024-07-11 18:28:41 +0530

    Show only category with products

--- app/[tenant]/[workspace]/(subapps)/shop/common/ui/components/featured-categories/featured-categories.tsx
+++ app/[tenant]/[workspace]/(subapps)/shop/common/ui/components/featured-categories/featured-categories.tsx
@@ -105,38 +105,42 @@ export function FeaturedCategories({
         )}
       </Swiper>
       <div className="container flex flex-col gap-6 mx-auto px-2 sm:px-0">
-        {featuredCategories.map(category => (
-          <Fragment key={category.id}>
-            <div className="flex justify-between items-center">
-              <h3 className="text-xl leading-7 font-medium">{category.name}</h3>
-              <div
-                className="flex gap-2 px-3 py-4 cursor-pointer"
-                onClick={() => handleCategoryClick(category)}>
-                <span className="leading-6 text-sm">See All</span>
-                <MdEast className="w-6 h-6" />
+        {featuredCategories.map(category =>
+          category?.products?.length ? (
+            <Fragment key={category.id}>
+              <div className="flex justify-between items-center">
+                <h3 className="text-xl leading-7 font-medium">
+                  {category.name}
+                </h3>
+                <div
+                  className="flex gap-2 px-3 py-4 cursor-pointer"
+                  onClick={() => handleCategoryClick(category)}>
+                  <span className="leading-6 text-sm">See All</span>
+                  <MdEast className="w-6 h-6" />
+                </div>
               </div>
-            </div>
-            <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-5">
-              {category?.products?.map((computedProduct: ComputedProduct) => {
-                const quantity = cart?.items?.find(
-                  (i: any) =>
-                    Number(i.product) === Number(computedProduct?.product.id),
-                )?.quantity;
+              <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-5">
+                {category.products.map((computedProduct: ComputedProduct) => {
+                  const quantity = cart?.items?.find(
+                    (i: any) =>
+                      Number(i.product) === Number(computedProduct?.product.id),
+                  )?.quantity;
 
-                return (
-                  <ProductCard
-                    key={computedProduct.product.id}
-                    product={computedProduct}
-                    quantity={quantity}
-                    onAdd={handleAddProduct}
-                    displayPrices={workspace?.config?.displayPrices}
-                    onClick={handleProductClick}
-                  />
-                );
-              })}
-            </div>
-          </Fragment>
-        ))}
+                  return (
+                    <ProductCard
+                      key={computedProduct.product.id}
+                      product={computedProduct}
+                      quantity={quantity}
+                      onAdd={handleAddProduct}
+                      displayPrices={workspace?.config?.displayPrices}
+                      onClick={handleProductClick}
+                    />
+                  );
+                })}
+              </div>
+            </Fragment>
+          ) : null,
+        )}
       </div>
     </div>
   );
