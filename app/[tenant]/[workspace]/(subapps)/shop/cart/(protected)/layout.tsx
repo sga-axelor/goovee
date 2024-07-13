0618d66006a9f3888c36117795f432d41ce7871e
npa-axelor <92723622+npa-axelor@users.noreply.github.com>
2024-07-13 08:33:11 +0530

    Fix redirection

--- app/[tenant]/[workspace]/(subapps)/shop/cart/(protected)/layout.tsx
+++ app/[tenant]/[workspace]/(subapps)/shop/cart/(protected)/layout.tsx
@@ -3,12 +3,21 @@ import {redirect} from 'next/navigation';
 
 // ---- CORE IMPORTS ---- //
 import {getSession} from '@/orm/auth';
+import {workspacePathname} from '@/utils/workspace';
 
-export default async function Layout({children}: {children: React.ReactNode}) {
+export default async function Layout({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: {tenant: string; workspace: string};
+}) {
   const session = await getSession();
 
+  const {workspaceURI} = workspacePathname(params);
+
   if (!session?.user) {
-    redirect('/auth/login');
+    redirect(`/auth/login?workspaceURI=${encodeURIComponent(workspaceURI)}`);
   }
 
   return <>{children}</>;
