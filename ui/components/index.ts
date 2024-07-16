3a882953fd974d4ad24c2d8157a3493e0945161f
sga-axelor <115212874+sga-axelor@users.noreply.github.com>
2024-07-16 11:46:35 +0530

    RM-81790: Remove mobile menu category export from main ui components

--- ui/components/index.ts
+++ ui/components/index.ts
@@ -194,4 +194,3 @@ export {
 } from './alert-dialog';
 export {Banner} from './banner';
 export {Pagination} from './custom-pagination';
-export {MobileMenuCategory} from '../../app/[tenant]/[workspace]/(subapps)/events/common/ui/components/mobile-menu-category';
