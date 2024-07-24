fdb51a7d361f9bf7a63545ebcb745474687ff1c2
sga-axelor <115212874+sga-axelor@users.noreply.github.com>
2024-07-24 16:22:31 +0530

    RM-82109: Update trigger for group action list

--- app/[tenant]/[workspace]/(subapps)/forum/common/ui/components/group-action-list/group-action-list.tsx
+++ app/[tenant]/[workspace]/(subapps)/forum/common/ui/components/group-action-list/group-action-list.tsx
@@ -34,59 +34,57 @@ export const GroupActionList = ({title}: {title: string}) => {
       </h1>
       <div className="flex flex-col gap-4">
         {Array.from({length: 6}).map((_, i) => (
-          <div key={i} className="flex flex-col gap-2">
-            <Collapsible>
-              <CollapsibleTrigger className="w-full">
-                <div className="w-full flex-shrink-0 flex justify-between items-center gap-2">
-                  <div className="flex items-center gap-2">
-                    <Avatar className="rounded-lg h-6 w-6 bg-red-400">
-                      {/* <AvatarImage src="/images/user.png" /> */}
-                    </Avatar>
-                    <p className="font-normal text-sm leading-5 line-clamp-1">
-                      {GROUP_NAME}
-                    </p>
-                  </div>
-                  <div className="flex items-center gap-2">
-                    <div className="px-1 bg-success/10 cursor-pointer rounded-sm leading-[15px]">
-                      <span className="text-success text-[10px]">2</span>
-                    </div>
-                    {i % 2 === 0 && (
-                      <MdOutlinePushPin className="cursor-pointer" />
-                    )}
-                    <MdMoreVert className="cursor-pointer" />
-                  </div>
+          <Collapsible key={i}>
+            <div className="w-full flex-shrink-0 flex justify-between items-center gap-2">
+              <div className="flex items-center gap-2">
+                <Avatar className="rounded-lg h-6 w-6 bg-red-400">
+                  {/* <AvatarImage src="/images/user.png" /> */}
+                </Avatar>
+                <p className="font-normal text-sm leading-5 line-clamp-1 cursor-pointer">
+                  {GROUP_NAME}
+                </p>
+              </div>
+              <div className="flex items-center gap-2">
+                <div className="px-1 bg-success/10 cursor-pointer rounded-sm leading-[15px]">
+                  <span className="text-success text-[10px]">2</span>
                 </div>
-              </CollapsibleTrigger>
-              <CollapsibleContent>
-                <div className="flex flex-col gap-2">
-                  <div className="flex items-center gap-[10px] px-2">
-                    <MdOutlineMarkChatRead className="w-4 h-4" />
-                    <span className="w-full text-xs leading-[18px] font-normal cursor-pointer">
-                      {MARK_AS_READ}
-                    </span>
-                  </div>
-                  <div className="flex items-center gap-[10px] px-2">
-                    <MdOutlinePushPin className="w-4 h-4" />
-                    <span className="w-full text-xs leading-[18px] font-normal cursor-pointer">
-                      {PIN}
-                    </span>
-                  </div>
-                  <div className="flex items-center gap-[10px] px-2">
-                    <MdNotificationsNone className="w-4 h-4" />
-                    <span className="w-full text-xs leading-[18px] font-normal cursor-pointer">
-                      {NOTIFICATIONS}
-                    </span>
-                  </div>
-                  <div className="flex items-center gap-[10px] px-2">
-                    <MdExitToApp className="w-4 h-4" />
-                    <span className="w-full text-xs leading-[18px] font-normal cursor-pointer">
-                      {LEAVE_THIS_GROUP}
-                    </span>
-                  </div>
+                {i % 2 === 0 && (
+                  <MdOutlinePushPin className="cursor-pointer w-4 h-4" />
+                )}
+                <CollapsibleTrigger>
+                  <MdMoreVert className="cursor-pointer" />
+                </CollapsibleTrigger>
+              </div>
+            </div>
+            <CollapsibleContent className="mt-2">
+              <div className="flex flex-col gap-2">
+                <div className="flex items-center gap-[10px] px-2">
+                  <MdOutlineMarkChatRead className="w-4 h-4" />
+                  <span className="w-full text-xs leading-[18px] font-normal cursor-pointer">
+                    {MARK_AS_READ}
+                  </span>
                 </div>
-              </CollapsibleContent>
-            </Collapsible>
-          </div>
+                <div className="flex items-center gap-[10px] px-2">
+                  <MdOutlinePushPin className="w-4 h-4" />
+                  <span className="w-full text-xs leading-[18px] font-normal cursor-pointer">
+                    {PIN}
+                  </span>
+                </div>
+                <div className="flex items-center gap-[10px] px-2">
+                  <MdNotificationsNone className="w-4 h-4" />
+                  <span className="w-full text-xs leading-[18px] font-normal cursor-pointer">
+                    {NOTIFICATIONS}
+                  </span>
+                </div>
+                <div className="flex items-center gap-[10px] px-2">
+                  <MdExitToApp className="w-4 h-4" />
+                  <span className="w-full text-xs leading-[18px] font-normal cursor-pointer">
+                    {LEAVE_THIS_GROUP}
+                  </span>
+                </div>
+              </div>
+            </CollapsibleContent>
+          </Collapsible>
         ))}
       </div>
     </div>
