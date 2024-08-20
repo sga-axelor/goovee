9c73041139b101e64bdf85c2e22198a3cbd7bfbf
npa-axelor <92723622+npa-axelor@users.noreply.github.com>
2024-08-20 12:59:03 +0530

    Create ticket form

--- app/[tenant]/[workspace]/(subapps)/ticketing/projects/[project-id]/tickets/create/form.tsx
+++ app/[tenant]/[workspace]/(subapps)/ticketing/projects/[project-id]/tickets/create/form.tsx
@@ -1,13 +1,57 @@
 'use client';
 
-// ---- CORE IMPORT ---- //
-import {useWorkspace} from '@/app/[tenant]/[workspace]/workspace-context';
-
 // ---- LOCAL IMPORT ---- //
 import {create} from './action';
 
+import {useRef} from 'react';
+import {zodResolver} from '@hookform/resolvers/zod';
+import {useForm} from 'react-hook-form';
+import {z} from 'zod';
+
+// ---- CORE IMPORTS ---- //
+import {useWorkspace} from '@/app/[tenant]/[workspace]/workspace-context';
+import {Button} from '@/ui/components/button';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from '@/ui/components/form';
+import {Input} from '@/ui/components/input';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/ui/components/select';
+import {i18n} from '@/lib/i18n';
+import {Textarea} from '@/ui/components';
+
+const formSchema = z.object({
+  subject: z.string().min(1, {message: 'Subject is required'}),
+  category: z.string(),
+  priority: z.string(),
+  contact: z.string(),
+  description: z.string(),
+});
+
 export default function TicketForm({ticket}: {ticket: any}) {
   const {workspaceURL} = useWorkspace();
+
+  const formRef = useRef<HTMLFormElement>(null);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      subject: '',
+      category: '',
+      priority: '',
+      contact: '',
+      description: '',
+    },
+  });
 
   const handleSubmit = () => {
     /**
@@ -16,6 +60,168 @@ export default function TicketForm({ticket}: {ticket: any}) {
      * Call create action with formdata and workspaceURL
      */
   };
+  const category = [
+    {
+      name: 'category',
+      value: 'category1',
+    },
+    {
+      name: 'category',
+      value: 'category2',
+    },
+    {
+      name: 'category',
+      value: 'category3',
+    },
+  ];
 
-  return <form></form>;
+  const priority = [
+    {
+      name: 'category',
+      value: 'Low',
+    },
+    {
+      name: 'category',
+      value: 'Medium',
+    },
+    {
+      name: 'category',
+      value: 'High',
+    },
+  ];
+
+  const contact = [
+    {
+      id: '1',
+      name: 'Contact 1',
+    },
+    {
+      id: '2',
+      name: 'Contact 2',
+    },
+    {
+      id: '3',
+      name: 'Contact 3',
+    },
+  ];
+  return (
+    <Form {...form}>
+      <form
+        ref={formRef}
+        onSubmit={handleSubmit}
+        className="space-y-8 container">
+        <FormField
+          control={form.control}
+          name="subject"
+          render={({field}) => (
+            <FormItem>
+              <FormLabel>{i18n.get('Subject')}*</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder={i18n.get('Enter your subject')}
+                  {...field}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="category"
+          render={({field}) => (
+            <FormItem>
+              <FormLabel>{i18n.get('Category')}</FormLabel>
+              <Select onValueChange={field.onChange} defaultValue={field.value}>
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue
+                      placeholder={i18n.get('Select your category')}
+                    />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  {category.map((category: any) => (
+                    <SelectItem value={category.value} key={category.value}>
+                      {category.value}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="priority"
+          render={({field}) => (
+            <FormItem>
+              <FormLabel>{i18n.get('Priority')}</FormLabel>
+              <Select onValueChange={field.onChange} defaultValue={field.value}>
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue
+                      placeholder={i18n.get('Select your priority')}
+                    />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  {priority.map((priority: any) => (
+                    <SelectItem value={priority.value} key={priority.value}>
+                      {priority.value}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="contact"
+          render={({field}) => (
+            <FormItem>
+              <FormLabel>{i18n.get('Contact')}</FormLabel>
+              <Select onValueChange={field.onChange} defaultValue={field.value}>
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue
+                      placeholder={i18n.get('Select your contact')}
+                    />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  {contact.map((contact: any) => (
+                    <SelectItem value={contact.id} key={contact.id}>
+                      {contact.name}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="description"
+          render={({field}) => (
+            <FormItem>
+              <FormLabel>{i18n.get('Ticket description')}</FormLabel>
+              <FormControl>
+                <Textarea
+                  placeholder={i18n.get('Enter ticket description')}
+                  {...field}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <Button type="submit" className="w-30 float-right" variant="success">
+          {i18n.get('Create a ticket')}
+        </Button>
+      </form>
+    </Form>
+  );
 }
