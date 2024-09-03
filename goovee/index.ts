19fa93de18787ce6adb0b8da7281910dad1a3c72
npa-axelor <92723622+npa-axelor@users.noreply.github.com>
2024-09-03 09:36:53 +0530

    fix: update lru cache types (#83595)

--- goovee/index.ts
+++ goovee/index.ts
@@ -1,12 +1,12 @@
 import axios from 'axios';
 import {LRUCache} from '@/lib/cache';
-import type {Tenant} from '@/types';
+import type {ID, Tenant} from '@/types';
 import {DEFAULT_TENANT} from '@/constants';
 import {GooveeClient, createClient} from './.generated/client';
 
-const cache = new LRUCache<string, GooveeClient>(10);
+const cache = new LRUCache<ID, GooveeClient>(10);
 
-async function getTenant(tenantId: string): Promise<Tenant | null> {
+async function getTenant(tenantId: ID): Promise<Tenant | null> {
   const tenant = await axios
     .get(`http://localhost:3000/api/tenant/${tenantId}`, {
       auth: {
@@ -19,7 +19,7 @@ async function getTenant(tenantId: string): Promise<Tenant | null> {
   return tenant;
 }
 
-export async function getClient(tenantId: string = DEFAULT_TENANT) {
+export async function getClient(tenantId: ID = DEFAULT_TENANT) {
   if (!tenantId) {
     throw new Error('No tenant.');
   }
