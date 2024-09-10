bf06a89f46d0e39d318ea2a67cd054956a585b18
npa-axelor <92723622+npa-axelor@users.noreply.github.com>
2024-09-10 15:36:39 +0530

    fix: update batch download with tenant (#83595)

--- app/api/tenant/[tenant]/download/route.ts
+++ app/api/tenant/[tenant]/download/route.ts
@@ -1,19 +1,36 @@
+import {getTenant} from '@/goovee';
 import axios from 'axios';
 import {NextResponse} from 'next/server';
 
-export async function POST(request: Request) {
+export async function POST(
+  request: Request,
+  {params}: {params: {tenant: string}},
+) {
+  let tenant;
+
+  try {
+    const result = await getTenant(params.tenant);
+    tenant = result?.tenant;
+  } catch (err) {}
+
+  if (!(tenant && tenant?.aos?.url && tenant?.aos?.auth)) {
+    return new NextResponse('Bad Request', {status: 400});
+  }
+
   const {model = 'com.axelor.dms.db.DMSFile', record} = await request.json();
 
+  const {aos} = tenant;
+
   const res = await axios.post(
-    `${process.env.NEXT_PUBLIC_AOS_URL}/ws/dms/download/batch`,
+    `${aos.url}/ws/dms/download/batch`,
     {
       model,
       records: [record.id],
     },
     {
       auth: {
-        username: process.env.BASIC_AUTH_USERNAME as string,
-        password: process.env.BASIC_AUTH_PASSWORD as string,
+        username: aos.auth.username,
+        password: aos.auth.password,
       },
     },
   );
@@ -22,7 +39,7 @@ export async function POST(request: Request) {
     const {batchId, batchName} = await res.data;
     if (batchId || batchName) {
       return download(
-        `${process.env.NEXT_PUBLIC_AOS_URL}/ws/dms/download/${batchId}?fileName=${batchName}`,
+        `${aos.url}/ws/dms/download/${batchId}?fileName=${batchName}`,
         batchName,
       );
     }
