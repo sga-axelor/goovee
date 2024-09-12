afe7be57b4614d72de3a36625d58aafc244f2dbe
npa-axelor <92723622+npa-axelor@users.noreply.github.com>
2024-09-12 15:27:11 +0530

    refactor: update file stream handling for reusability (#83595)

--- app/api/tenant/[tenant]/download/[id]/route.ts
+++ app/api/tenant/[tenant]/download/[id]/route.ts
@@ -3,7 +3,76 @@ import {getTenant} from '@/goovee';
 import {NextRequest, NextResponse} from 'next/server';
 import type {ReadableOptions} from 'stream';
 
-function streamFile(
+export async function findFile({
+  id,
+  meta,
+  tenant: tenantId,
+}: {
+  id: string;
+  meta?: boolean;
+  tenant: string;
+}) {
+  if (!id && tenantId) {
+    return null;
+  }
+
+  let tenant, client;
+
+  try {
+    const result = await getTenant(tenantId);
+
+    tenant = result?.tenant;
+    client = result?.client;
+  } catch (err) {
+    return null;
+  }
+
+  if (!(client && tenant && tenant?.aos?.storage)) {
+    return null;
+  }
+
+  const storage = tenant?.aos?.storage;
+
+  let record, filePath, fileName, fileType;
+
+  if (meta) {
+    record = await client.aOSMetaFile.findOne({
+      where: {id},
+    });
+
+    if (!record) {
+      return null;
+    }
+
+    filePath = `${storage}/${record?.filePath}`;
+    fileName = record?.fileName;
+    fileType = record?.fileType;
+  } else {
+    record = await client.aOSDMSFile.findOne({
+      where: {id},
+      select: {
+        metaFile: true,
+      },
+    });
+
+    if (!record) {
+      return null;
+    }
+
+    filePath = `${storage}/${record?.metaFile?.filePath}`;
+    fileName = record?.metaFile?.fileName;
+    fileType = record?.metaFile?.fileType;
+  }
+
+  return {
+    record,
+    fileName,
+    filePath,
+    fileType,
+  };
+}
+
+export function createStream(
   path: string,
   options?: ReadableOptions,
 ): ReadableStream<Uint8Array> {
@@ -25,62 +94,21 @@ function streamFile(
   });
 }
 
-export async function GET(
-  request: NextRequest,
-  {params}: {params: {tenant: string; id: string}},
-) {
-  const {id} = params;
-
-  let tenant, client;
-
-  try {
-    const result = await getTenant(params.tenant);
-    tenant = result?.tenant;
-    client = result?.client;
-  } catch (err) {}
-
-  if (!(client && tenant && tenant?.aos?.storage)) {
-    return new NextResponse('Bad Request', {status: 400});
-  }
-
-  const storage = tenant?.aos?.storage;
-
-  const searchParams = request.nextUrl.searchParams;
-  const meta = searchParams.get('meta') === 'true';
-
-  let record, filePath, fileName, fileType;
-
-  if (meta) {
-    record = await client.aOSMetaFile.findOne({
-      where: {id},
-    });
-
-    if (!record) {
-      return new NextResponse('File not found', {status: 404});
-    }
-
-    filePath = `${storage}/${record?.filePath}`;
-    fileName = record?.fileName;
-    fileType = record?.fileType;
-  } else {
-    record = await client.aOSDMSFile.findOne({
-      where: {id},
-      select: {
-        metaFile: true,
-      },
-    });
-
-    if (!record) {
-      return new NextResponse('File not found', {status: 404});
-    }
-
-    filePath = `${storage}/${record?.metaFile?.filePath}`;
-    fileName = record?.metaFile?.fileName;
-    fileType = record?.metaFile?.fileType;
+export async function streamFile({
+  fileName,
+  filePath,
+  fileType,
+}: {
+  fileName: string;
+  filePath: string;
+  fileType: string;
+}) {
+  if (!(fileName && filePath && fileType)) {
+    return new NextResponse('Bad request', {status: 400});
   }
 
   try {
-    const data: ReadableStream<Uint8Array> = streamFile(filePath);
+    const data: ReadableStream<Uint8Array> = createStream(filePath);
     const stats: Stats = await fs.promises.stat(filePath);
 
     const res = new NextResponse(data, {
@@ -96,4 +124,26 @@ export async function GET(
   } catch (err) {
     return new NextResponse('Error downloading file', {status: 500});
   }
+}
+
+export async function GET(
+  request: NextRequest,
+  {params}: {params: {tenant: string; id: string}},
+) {
+  const {id, tenant} = params;
+
+  const searchParams = request.nextUrl.searchParams;
+  const meta = searchParams.get('meta') === 'true';
+
+  const file = await findFile({
+    id,
+    meta,
+    tenant,
+  });
+
+  if (!file) {
+    return new NextResponse('File not found', {status: 404});
+  }
+
+  return streamFile(file);
 }
