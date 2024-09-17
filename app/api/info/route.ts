db5c5868efe00cbe49c51d4ef47f04499e423060
npa-axelor <92723622+npa-axelor@users.noreply.github.com>
2024-09-17 15:38:52 +0530

    fix: add tenancy information (#83595)

--- app/api/info/route.ts
+++ app/api/info/route.ts
@@ -1,10 +1,12 @@
 import {NextResponse} from 'next/server';
 import goovee from '@/package.json';
+import {manager} from '@/tenant';
 
 export function GET() {
   return NextResponse.json({
     name: 'Goovee',
     version: goovee?.version,
     date: new Date().toISOString(),
+    tenancy: manager.getType(),
   });
 }
