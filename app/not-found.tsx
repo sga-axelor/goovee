97bc29f55c3ce85768bde7ddf6aa5c22239a616e
npa-axelor <92723622+npa-axelor@users.noreply.github.com>
2024-09-17 15:56:14 +0530

    fix: allow custom not found message (#83595)

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,16 +1,24 @@
+'use client';
+
 import Link from 'next/link';
+import {useSearchParams} from 'next/navigation';
 import {Button} from '@/ui/components';
 import {i18n} from '@/i18n';
 
 export default function NotFound() {
+  const searchParams = useSearchParams();
+  const searchParamMessage = searchParams.get('message');
+
+  const message = searchParamMessage
+    ? decodeURIComponent(searchParamMessage)
+    : i18n.get('Could not find the requested resource');
+
   return (
     <div className="flex items-center justify-center h-screen">
       <div className="space-y-2">
         <div className="space-y-1">
           <h2 className="text-3xl">404 | {i18n.get('Not Found')}</h2>
-          <p className="text-muted-foreground">
-            {i18n.get('Could not find the requested resource')}
-          </p>
+          <p className="text-muted-foreground">{message}</p>
         </div>
         <div>
           <Link href="/">
