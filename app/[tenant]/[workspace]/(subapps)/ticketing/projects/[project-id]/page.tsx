9ea24531cde473b5902a533895ccdc67ca038721
kac-axelor <215158475+kac-axelor@users.noreply.github.com>
2024-09-17 13:46:18 +0530

    refactor: improve cards (#82966)

--- app/[tenant]/[workspace]/(subapps)/ticketing/projects/[project-id]/page.tsx
+++ app/[tenant]/[workspace]/(subapps)/ticketing/projects/[project-id]/page.tsx
@@ -193,17 +193,17 @@ export default async function Page({
 type TicketCardProps = {
   label: string;
   count: Promise<number>;
-  href?: string;
-  icon?: IconType;
-  iconClassName?: string;
+  href: string;
+  icon: IconType;
+  iconClassName: string;
 };
 
 async function TicketCard(props: TicketCardProps) {
   const {label, icon: Icon, count: countPromise, href, iconClassName} = props;
   const count = await countPromise;
 
-  const content = (
-    <>
+  return (
+    <Link href={href} className="flex items-center gap-6 px-4 h-full">
       <div
         className={cn(
           'flex items-center justify-center h-10 w-10 rounded-full',
@@ -215,18 +215,8 @@ async function TicketCard(props: TicketCardProps) {
         <h3 className="text-[28px] font-semibold">{count}</h3>
         <p className="text-sm font-semibold">{label}</p>
       </div>
-    </>
+    </Link>
   );
-
-  const className = 'flex items-center gap-6 px-4 h-full';
-  if (href) {
-    return (
-      <Link href={href} className={className}>
-        {content}
-      </Link>
-    );
-  }
-  return <div className={className}>{content}</div>;
 }
 
 function TicketCardSkeleton() {
