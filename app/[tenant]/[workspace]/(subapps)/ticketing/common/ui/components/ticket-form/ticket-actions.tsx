db0967c320a66b89f5a7b7a8079499002ae9433a
kac-axelor <215158475+kac-axelor@users.noreply.github.com>
2024-09-19 13:20:50 +0530

    style: change alert to warning (#82966)

--- app/[tenant]/[workspace]/(subapps)/ticketing/common/ui/components/ticket-form/ticket-actions.tsx
+++ app/[tenant]/[workspace]/(subapps)/ticketing/common/ui/components/ticket-form/ticket-actions.tsx
@@ -160,7 +160,7 @@ export function RelatedTicketsHeader(props: {
         )}
       </div>
       {showAlert && (
-        <Alert variant="success" className="group">
+        <Alert variant="warning" className="group">
           <button
             className="ring-0 absolute right-2 top-2 rounded-md cursor-pointer p-1 text-foreground/50 lg:opacity-0 transition-opacity hover:text-foreground focus:opacity-100 focus:outline-none group-hover:opacity-100"
             onClick={() => setShowAlert(false)}>
