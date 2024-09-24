6e76f45e93dcb68f47b9589548b78334d9f8724b
sga-axelor <115212874+sga-axelor@users.noreply.github.com>
2024-09-24 18:56:26 +0530

    feat: refactor useComments hook to include createComment functionality

--- ui/hooks/use-comments/use-comments.tsx
+++ ui/hooks/use-comments/use-comments.tsx
@@ -3,21 +3,38 @@
 import {useState, useEffect, useCallback, useMemo} from 'react';
 
 // ---- CORE IMPORTS ---- //
-import {fetchComments} from '@/app/actions/comment';
+import {createComment, fetchComments} from '@/app/actions/comment';
 import {DEFAULT_COMMENTS_LIMIT, DEFAULT_PAGE} from '@/constants';
-import {ID, ModelType} from '@/types';
+import {CommentResponse, ID, ModelType} from '@/types';
 import {useWorkspace} from '@/app/[tenant]/[workspace]/workspace-context';
+import {useToast} from '@/ui/hooks';
+import {i18n} from '@/lib/i18n';
+
+interface UseCommentsProps {
+  sortBy: string;
+  model: {id: ID};
+  modelType: ModelType;
+  seeMore?: boolean;
+}
+
+interface CommentFetchParams {
+  page: number;
+  sortBy: string;
+}
+
+interface HandleCommentParams {
+  formData: any;
+  values: any;
+  parent?: number | null;
+}
 
 export function useComments({
   sortBy: sortByProp,
   model,
   modelType,
-}: {
-  sortBy: string;
-  model: {id: ID};
-  modelType: ModelType;
-}) {
-  const [comments, setComments] = useState<any>([]);
+  seeMore,
+}: UseCommentsProps) {
+  const [comments, setComments] = useState<any[]>([]);
   const [page, setPage] = useState(DEFAULT_PAGE);
   const [sortBy, setSortBy] = useState(sortByProp);
   const [loading, setLoading] = useState(true);
@@ -25,56 +42,81 @@ export function useComments({
   const [fetching, setFetching] = useState(false);
 
   const {workspaceURL} = useWorkspace();
+  const {toast} = useToast();
 
   const getComments = useCallback(
-    async ({
-      page,
-      sortBy,
-      disableAppend,
-    }: {
-      page: number;
-      sortBy: string;
-      disableAppend?: boolean;
-    }) => {
+    async ({page, sortBy}: CommentFetchParams) => {
       setFetching(true);
-
       try {
         const response: any = await fetchComments({
           model: {id: model.id},
-          sort: sortBy,
-          //TODO: add optional limit based on app
-          limit: DEFAULT_COMMENTS_LIMIT,
-          page,
           type: modelType,
+          sort: sortBy,
+          limit: seeMore ? DEFAULT_COMMENTS_LIMIT : undefined,
+          page,
           workspaceURL,
         });
 
         if (response.success) {
-          const {data = []} = response;
-
-          if (page > 1 && !disableAppend) {
-            setComments((prevComments: any) => [...prevComments, ...data]);
-          } else {
-            setComments(data);
-          }
-          setTotal(data[0]?._count || 0);
-          setFetching(false);
-
-          return response;
+          const {data = [], total} = response;
+          setComments(prevComments =>
+            page > 1 ? [...prevComments, ...data] : data,
+          );
+          setTotal(total || 0);
         } else {
-          setFetching(false);
-          return {error: true, message: 'Something went wrong!'};
+          throw new Error('Something went wrong!');
         }
-        return response;
-      } catch (error) {
+      } catch (error: any) {
+        console.error('Fetch error:', error);
+        toast({
+          variant: 'destructive',
+          title: i18n.get(
+            error.message || 'An error occurred while fetching comments.',
+          ),
+        });
+      } finally {
         setFetching(false);
-        return {
-          error: true,
-          message: 'An error occurred while fetching comments.',
-        };
       }
     },
-    [model, modelType, workspaceURL],
+    [model.id, modelType, seeMore, workspaceURL, toast],
+  );
+
+  const handleRefresh = useCallback(async () => {
+    await getComments({page, sortBy});
+    setLoading(false);
+  }, [getComments, page, sortBy]);
+
+  const handleComment = useCallback(
+    async ({formData, values, parent = null}: HandleCommentParams) => {
+      try {
+        const response: CommentResponse = await createComment({
+          formData,
+          values,
+          workspaceURL,
+          modelID: model.id,
+          parentId: parent,
+          type: ModelType.forum,
+          tracks: [],
+        });
+
+        if (response.success) {
+          handleRefresh();
+          toast({
+            variant: 'success',
+            title: i18n.get('Comment created successfully.'),
+          });
+        } else {
+          throw new Error(response.message || 'Error creating comment');
+        }
+      } catch (error: any) {
+        console.error('Submission error:', error);
+        toast({
+          variant: 'destructive',
+          title: i18n.get(error.message || 'An unexpected error occurred.'),
+        });
+      }
+    },
+    [model.id, toast, workspaceURL, handleRefresh],
   );
 
   useEffect(() => {
@@ -83,18 +125,15 @@ export function useComments({
   }, [sortByProp]);
 
   useEffect(() => {
-    getComments({page, sortBy}).finally(() => {
-      setLoading(false);
-    });
+    getComments({page, sortBy}).finally(() => setLoading(false));
   }, [page, sortBy]);
 
   const hasMore = useMemo(() => comments.length < total, [comments, total]);
 
   const loadMore = useCallback(() => {
-    if (!hasMore) {
-      return;
+    if (hasMore) {
+      setPage(prevPage => prevPage + 1);
     }
-    setPage(page => page + 1);
   }, [hasMore]);
 
   return useMemo(
@@ -106,7 +145,19 @@ export function useComments({
       loading,
       getComments,
       loadMore,
+      onCreate: handleComment,
+      onRefresh: handleRefresh,
     }),
-    [comments, total, hasMore, fetching, loading, getComments, loadMore],
+    [
+      comments,
+      total,
+      hasMore,
+      fetching,
+      loading,
+      getComments,
+      loadMore,
+      handleComment,
+      handleRefresh,
+    ],
   );
 }
