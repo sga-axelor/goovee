8da59ae11d1d61bf6f75b8d75719c715d1bc0e84
kac-axelor <215158475+kac-axelor@users.noreply.github.com>
2024-10-13 19:05:40 +0530

    style: keep bottom margin even in large screen (#82966)

--- app/[tenant]/[workspace]/(subapps)/ticketing/layout.tsx
+++ app/[tenant]/[workspace]/(subapps)/ticketing/layout.tsx
@@ -34,5 +34,5 @@ export default async function Layout({
 
   if (!subapp) return notFound();
 
-  return <div className="lg:mb-0 mb-[72px]">{children}</div>;
+  return <div className="mb-[72px]">{children}</div>;
 }
