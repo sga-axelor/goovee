839a791f20991f194426a6bb3fc72278965ca0ad
sga-axelor <115212874+sga-axelor@users.noreply.github.com>
2024-10-25 15:11:52 +0530

    feat: add dynamic category colors to the event category badge

--- app/[tenant]/[workspace]/(subapps)/events/common/ui/components/event-card-badges/event-card-badges.tsx
+++ app/[tenant]/[workspace]/(subapps)/events/common/ui/components/event-card-badges/event-card-badges.tsx
@@ -3,15 +3,32 @@ import {Badge} from '@/ui/components/badge';
 
 // ---- LOCAL IMPORTS ---- //
 import {EventCardBadgesProps} from '@/subapps/events/common/ui/components';
+import {getColorStyles} from '@/subapps/events/common/utils';
 
 export const EventCardBadges = ({categories}: EventCardBadgesProps) => {
   return (
     <div className="flex items-center flex-wrap gap-y-2 gap-x-4 ">
-      {categories?.map(category => {
+      {categories?.map((category: any) => {
+        const {
+          backgroundColor,
+          textColor,
+          hoverBackgroundColor,
+          hoverTextColor,
+        } = getColorStyles(category.color, true);
+
         return (
           <Badge
             key={category?.id}
-            className={`rounded-2xl shrink-0 px-2 py-1 text-[0.625rem] font-normal w-fit]`}>
+            className={`rounded-2xl shrink-0 px-2 py-1 text-[0.625rem] font-normal w-fit]`}
+            style={{backgroundColor, color: textColor}}
+            onMouseEnter={e => {
+              e.currentTarget.style.backgroundColor = hoverBackgroundColor;
+              e.currentTarget.style.color = hoverTextColor;
+            }}
+            onMouseLeave={e => {
+              e.currentTarget.style.backgroundColor = backgroundColor;
+              e.currentTarget.style.color = textColor;
+            }}>
             {category?.name}
           </Badge>
         );
