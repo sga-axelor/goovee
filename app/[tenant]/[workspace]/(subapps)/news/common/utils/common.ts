744321bca6a3329e9bb6f810af9c98b886c428bd
npa-axelor <92723622+npa-axelor@users.noreply.github.com>
2024-10-30 12:29:42 +0530

    chore: remove dead code

--- app/[tenant]/[workspace]/(subapps)/news/common/utils/common.ts
+++ app/[tenant]/[workspace]/(subapps)/news/common/utils/common.ts
@@ -33,13 +33,6 @@ export function getPublishedLabel(dateString: any) {
   return TIME_UNITS.NOW;
 }
 
-export function getImageURL(id?: string | number) {
-  if (!id) {
-    return `/images/no-image.png`;
-  }
-
-  return `${process.env.NEXT_PUBLIC_HOST}/api/download/${id}?meta=true`;
-}
 
 export function parseDate(dateString: any) {
   const date = moment(dateString);
