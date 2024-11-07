eebc0334fd6e1e3bdff985dfba44b810b2d12f70
sga-axelor <115212874+sga-axelor@users.noreply.github.com>
2024-11-07 12:37:21 +0530

    feat: add group name to the search item in forum search

--- app/[tenant]/[workspace]/(subapps)/forum/common/ui/components/search-item/search-item.tsx
+++ app/[tenant]/[workspace]/(subapps)/forum/common/ui/components/search-item/search-item.tsx
@@ -7,15 +7,15 @@ export function SearchItem({
   result: any;
   onClick: (result: {id: string; title: string}) => void;
 }) {
-  const {id, title} = result;
+  const {id, title, forumGroup} = result;
   return (
     <div
       key={id}
       onClick={() => onClick(result)}
       className="flex flex-col gap-2 cursor-pointer">
-      <div className="flex justify-between">
-        <div className="text-sm font-semibold">{title}</div>
-        <div className="flex gap-2 h-max"></div>
+      <div className="flex justify-between text-sm">
+        <div className="font-semibold">{title}</div>
+        <div className="flex gap-2 h-max">{forumGroup.name}</div>
       </div>
     </div>
   );
