da494971048bbb6172b22fcc959d7b4f254a1672
npa-axelor <92723622+npa-axelor@users.noreply.github.com>
2024-11-07 17:03:33 +0530

    style: update apps layout (#85887)

---
+++ app/[tenant]/[workspace]/account/apps/content.tsx
@@ -0,0 +1,52 @@
+'use client';
+
+import Link from 'next/link';
+
+// ---- CORE IMPORTS ---- //
+import {i18n} from '@/i18n';
+import {useWorkspace} from '@/app/[tenant]/[workspace]/workspace-context';
+import {SUBAPP_PAGE} from '@/constants';
+import {Icon} from '@/ui/components';
+
+export default function Content({subapps}: {subapps: any}) {
+  const {workspaceURI} = useWorkspace();
+
+  return (
+    <>
+      <div className="flex flex-col gap-10">
+        {subapps
+          .filter((app: any) => app.installed && app.showInMySpace)
+          .sort(
+            (app1: any, app2: any) =>
+              app1.orderForMySpaceMenu - app2.orderForMySpaceMenu,
+          )
+          .reverse()
+          .map(({code, name, icon, color, background}: any) => {
+            const page = SUBAPP_PAGE[code as keyof typeof SUBAPP_PAGE] || '';
+            return (
+              <Link
+                key={code}
+                href={`${workspaceURI}/${code}${page}`}
+                className="no-underline">
+                <div className="p-6 rounded-lg border bg-card text-card-foreground">
+                  <div className="flex items-center gap-2">
+                    <div
+                      className="w-[4.5rem] h-[4.5rem] p-4 rounded-lg flex items-center justify-center"
+                      style={{
+                        background,
+                        color,
+                      }}>
+                      {icon ? <Icon name={icon} className="text-2xl" /> : null}
+                    </div>
+                    <p className="text-lg font-semibold mb-0">
+                      {i18n.get(name)}
+                    </p>
+                  </div>
+                </div>
+              </Link>
+            );
+          })}
+      </div>
+    </>
+  );
+}
---
+++ app/[tenant]/[workspace]/account/apps/page.tsx
@@ -0,0 +1,39 @@
+import {notFound} from 'next/navigation';
+
+// ---- CORE IMPORTS ---- //
+import {getSession} from '@/auth';
+import {findWorkspace, findSubapps} from '@/orm/workspace';
+import {clone} from '@/utils';
+import {workspacePathname} from '@/utils/workspace';
+
+// ---- LOCAL IMPORTS ---- //
+import Content from './content';
+
+export default async function Account({
+  params,
+}: {
+  params: {tenant: string; workspace: string};
+}) {
+  const {tenant} = params;
+  const session = await getSession();
+
+  if (!session) return notFound();
+
+  const {workspaceURL} = workspacePathname(params);
+
+  const workspace = await findWorkspace({
+    user: session?.user,
+    url: workspaceURL,
+    tenantId: tenant,
+  }).then(clone);
+
+  if (!workspace) return notFound();
+
+  const subapps = await findSubapps({
+    url: workspace.url,
+    user: session?.user,
+    tenantId: tenant,
+  });
+
+  return <Content subapps={subapps} />;
+}
