5592ccbde1b479cc1e922d3b41d47d5770a398e9
sga-axelor <115212874+sga-axelor@users.noreply.github.com>
2024-11-12 17:23:45 +0530

    feat: add user session check for Survey app (#86313)

--- app/[tenant]/[workspace]/(subapps)/survey/page.tsx
+++ app/[tenant]/[workspace]/(subapps)/survey/page.tsx
@@ -1,6 +1,16 @@
+import {notFound} from 'next/navigation';
+
+// ---- CORE IMPORTS ---- //
+import {getSession} from '@/auth';
+
 // ---- LOCAL IMPORTS ---- //
 import Content from './content';
 
 export default async function Page() {
+  const session = await getSession();
+  const user = session?.user;
+
+  if (!user) return notFound();
+
   return <Content />;
 }
