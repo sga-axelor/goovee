13efa4ff305d92d0cd5dfe84d7a6b566f1a74a0f
npa-axelor <92723622+npa-axelor@users.noreply.github.com>
2024-11-15 18:54:59 +0530

    feat: create cards (#86940)

--- app/[tenant]/[workspace]/(subapps)/directory/page.tsx
+++ app/[tenant]/[workspace]/(subapps)/directory/page.tsx
@@ -8,8 +8,23 @@ import {clone} from '@/utils';
 import {workspacePathname} from '@/utils/workspace';
 import {getImageURL} from '@/utils/files';
 
+import {
+  MdOutlineSupervisedUserCircle,
+  MdOutlineDiamond,
+  MdFoodBank,
+  MdAllInbox,
+  MdMoney,
+  MdOutlineMedicalServices,
+  MdOutlineSmartphone,
+} from 'react-icons/md';
+import {TbTool} from 'react-icons/tb';
+
 // ---- LOCAL IMPORTS ---- //
 import Hero from './hero';
+import {IconType} from 'react-icons';
+import Link from 'next/link';
+import {Swipe} from '../ticketing/common/ui/components/swipe';
+import {cn} from '@/utils/css';
 import {Map} from './common/ui/components/map';
 import {Sort} from './common/ui/components/sort';
 
@@ -44,6 +59,48 @@ export default async function Page({
     ? `url(${getImageURL(workspace.config.ticketHeroBgImage.id, tenant)})`
     : IMAGE_URL;
 
+  const cards = [
+    {
+      icon: MdOutlineSupervisedUserCircle,
+      label: 'Services',
+      iconClassName: 'bg-palette-purple text-palette-purple-dark',
+    },
+    {
+      icon: MdOutlineDiamond,
+      label: 'Luxury',
+      iconClassName: 'bg-palette-yellow text-palette-yellow-dark',
+    },
+    {
+      icon: MdFoodBank,
+      label: 'Agribusiness',
+      iconClassName: 'bg-palette-pink text-palette-pink-dark',
+    },
+    {
+      icon: MdAllInbox,
+      label: 'Wholesales',
+      iconClassName: 'text-success bg-success-light',
+    },
+    {
+      icon: TbTool,
+      label: 'Industry',
+      iconClassName: 'bg-palette-purple text-palette-purple-dark',
+    },
+    {
+      icon: MdMoney,
+      label: 'Bank',
+      iconClassName: 'bg-palette-blue text-palette-blue-dark',
+    },
+    {
+      icon: MdOutlineMedicalServices,
+      label: 'Medical',
+      iconClassName: 'bg-palette-purple text-palette-purple-dark',
+    },
+    {
+      icon: MdOutlineSmartphone,
+      label: 'Communication',
+      iconClassName: 'bg-palette-yellow text-palette-yellow-dark',
+    },
+  ].map(props => <DirectoryCards {...props} key={props.label} />);
   return (
     <>
       <Hero
@@ -53,21 +110,49 @@ export default async function Page({
         image={imageURL}
         tenantId={tenant}
       />
-      <div className="container flex has-[.expand]:flex-col gap-4 mt-4">
-        <aside className="space-y-4">
-          <Map showExpand markers={markers} />
-          <Sort />
-        </aside>
-        <main className="grow flex flex-col gap-4">
-          {Array.from({length: 10}).map((_, index) => (
-            <div
-              key={index}
-              className="flex justify-center items-center p-4 h-40 bg-blue-400">
-              cards
-            </div>
-          ))}
-        </main>
+      <div>
+        <Swipe
+          items={cards}
+          className="flex justify-center items-center !w-[127px] !h-[100px] mb-10 mt-5 pt-5 space-y-2"
+        />
+        <div className="flex has-[.expand]:flex-col gap-4 mt-4">
+          <aside className="space-y-4">
+            <Map showExpand markers={markers} />
+            <Sort />
+          </aside>
+          <main className="grow flex flex-col gap-4">
+            {Array.from({length: 10}).map((_, index) => (
+              <div
+                key={index}
+                className="flex justify-center items-center p-4 h-40 bg-blue-400">
+                cards
+              </div>
+            ))}
+          </main>
+        </div>
       </div>
     </>
+  );
+}
+
+type DirectoryCardsProps = {
+  label: string;
+  icon: IconType;
+  iconClassName: string;
+};
+function DirectoryCards(props: DirectoryCardsProps) {
+  const {label, icon: Icon, iconClassName} = props;
+  return (
+    <Link href="">
+      <div
+        className={cn(
+          iconClassName,
+          'flex items-center justify-center h-10 w-10 rounded-full m-auto',
+        )}>
+        <Icon className="h-8 w-8 " />
+      </div>
+
+      <p className="text-sm font-semibold text-center">{label}</p>
+    </Link>
   );
 }
