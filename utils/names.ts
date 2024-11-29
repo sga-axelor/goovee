3f658befb90f4a0141328dd71801aac36a06e01f
npa-axelor <92723622+npa-axelor@users.noreply.github.com>
2024-11-29 09:45:20 +0530

    feat: add utils for name initials

--- utils/names.ts
+++ utils/names.ts
@@ -6,3 +6,16 @@ export function toTitleCase(name: string, allUpper = false) {
   const res = startCase(name);
   return allUpper ? res : capitalize(res);
 }
+
+export function getInitials(fullName?: string) {
+  if (!fullName) return '';
+  
+  const nameParts = fullName.split(' ');
+
+  const firstInitial = nameParts[0].charAt(0).toUpperCase();
+  const lastInitial = nameParts[nameParts.length - 1].charAt(0).toUpperCase();
+
+  const initials = firstInitial + lastInitial;
+
+  return initials;
+}
