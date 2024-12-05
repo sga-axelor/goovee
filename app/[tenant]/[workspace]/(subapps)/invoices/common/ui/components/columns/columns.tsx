17008b2b52a4b50b087ac382e5f562c2b51a269b
sga-axelor <115212874+sga-axelor@users.noreply.github.com>
2024-12-05 10:52:10 +0530

    style: update pay button styles for invoice list

--- app/[tenant]/[workspace]/(subapps)/invoices/common/ui/components/columns/columns.tsx
+++ app/[tenant]/[workspace]/(subapps)/invoices/common/ui/components/columns/columns.tsx
@@ -67,7 +67,7 @@ export const UnpaidColumns = (allowInvoicePayment?: boolean) => {
       content: () => (
         <Button
           className={cn(
-            'bg-success hover:bg-success-dark flex items-center justify-center gap-2 w-full rounded-[0.375rem] p-1.5 text-base',
+            'h-9 bg-success hover:bg-success-dark flex items-center justify-center gap-2 w-full rounded-[0.375rem] p-1.5 text-base',
             {
               hidden: !allowInvoicePayment,
             },
