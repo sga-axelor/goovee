858f012f3106adafc208f0da19c88e87849d773f
sga-axelor <115212874+sga-axelor@users.noreply.github.com>
2024-12-12 19:25:00 +0530

    feat: create a common layout for quotations related addresses pages (#87789)

---
+++ app/[tenant]/[workspace]/(subapps)/quotations/[id]/address/layout.tsx
@@ -0,0 +1,79 @@
+import {notFound} from 'next/navigation';
+
+// ---- CORE IMPORTS ---- //
+import {Container} from '@/ui/components';
+import {getSession} from '@/auth';
+import {workspacePathname} from '@/utils/workspace';
+import {SUBAPP_CODES} from '@/constants';
+import {getTranslation} from '@/i18n/server';
+import {findSubappAccess, findWorkspace} from '@/orm/workspace';
+import {clone} from '@/utils';
+import type {User} from '@/types';
+
+// ---- LOCAL IMPORTS ---- //
+import {findQuotation} from '@/subapps/quotations/common/orm/quotations';
+import {getWhereClause} from '@/subapps/quotations/common/utils/quotations';
+
+export default async function Layout({
+  params,
+  children,
+}: {
+  params: {
+    id: string;
+    tenant: string;
+    workspace: string;
+  };
+  children: React.ReactNode;
+}) {
+  const {id, tenant} = params;
+
+  const session = await getSession();
+  const user: any = session?.user;
+
+  const {workspaceURL} = workspacePathname(params);
+
+  const workspace: any = await findWorkspace({
+    user: session?.user,
+    url: workspaceURL,
+    tenantId: tenant,
+  }).then(clone);
+
+  if (!workspace) {
+    return notFound();
+  }
+
+  const subapp = await findSubappAccess({
+    code: SUBAPP_CODES.quotations,
+    user,
+    url: workspaceURL,
+    tenantId: tenant,
+  });
+
+  const {id: userId, isContact, mainPartnerId} = user as User;
+
+  const where = getWhereClause(
+    isContact as boolean,
+    subapp?.role,
+    userId,
+    mainPartnerId as string,
+  );
+
+  const quotation = await findQuotation({
+    id,
+    tenantId: tenant,
+    params: {where},
+  }).then(clone);
+
+  if (!quotation) {
+    return notFound();
+  }
+
+  return (
+    <div className="mt-2">
+      <Container
+        title={`${await getTranslation('Quotation')} ${quotation.saleOrderSeq}`}>
+        {children}
+      </Container>
+    </div>
+  );
+}
