1701c861dc6e2a9ce2088b408a5ca61b264d7448
sga-axelor <115212874+sga-axelor@users.noreply.github.com>
2024-12-12 16:19:54 +0530

    feat: update contacts UI for quotations (#88431)

--- app/[tenant]/[workspace]/(subapps)/quotations/common/ui/components/contact-details/contacts-details.tsx
+++ app/[tenant]/[workspace]/(subapps)/quotations/common/ui/components/contact-details/contacts-details.tsx
@@ -25,48 +25,44 @@ export const ContactDetails = ({
       <div className="flex flex-col gap-4">
         <h4 className="text-xl font-medium mb-0">{i18n.get('Contact')}</h4>
         <div className="flex flex-col gap-4 border rounded-lg p-4">
-          <div className="flex flex-col gap-4">
-            <h6 className="font-semibold text-base">
-              {i18n.get('Invoicing address')}
-            </h6>
-            <div className="flex flex-col gap-4">
+          <div className="flex gap-4">
+            <div className="flex-1 flex flex-col gap-4">
               <h6 className="font-semibold text-base">
-                {clientPartner?.fullName}, {company?.name}
+                {i18n.get('Invoicing address')}
               </h6>
-              <div className="text-sm leading-[1.313rem]">
-                <p>{mainInvoicingAddress?.addressl4}</p>
-                <p>{billingAddressCity}</p>
-                <p>{mainInvoicingAddress?.zip}</p>
-                <p>{mainInvoicingAddress?.country?.name}</p>
+              <div className="flex flex-col gap-4">
+                <h6 className="font-semibold text-base">
+                  {clientPartner?.fullName}, {company?.name}
+                </h6>
+                <div className="text-sm leading-[1.313rem]">
+                  <p>{mainInvoicingAddress?.addressl4}</p>
+                  <p>{billingAddressCity}</p>
+                  <p>{mainInvoicingAddress?.zip}</p>
+                  <p>{mainInvoicingAddress?.country?.name}</p>
+                </div>
               </div>
             </div>
-            <div className="flex">
-              <Button
-                className="bg-white border border-success text-success hover:bg-success hover:text-white rounded-md font-medium text-base py-1.5 px-3"
-                onClick={onAddressSelection}>
-                {i18n.get('Choose another address')}
-              </Button>
+            <div className="flex-1 flex flex-col gap-4">
+              <h6 className="font-semibold">{i18n.get('Delivery address')}</h6>
+              <div className="flex flex-col gap-4">
+                <h6 className="font-semibold text-base">
+                  {clientPartner?.fullName}, {company?.name}
+                </h6>
+                <div className="text-sm leading-[1.313rem]">
+                  <p>{deliveryAddress?.addressl4}</p>
+                  <p>{deliveryAddressCity}</p>
+                  <p>{deliveryAddress?.zip}</p>
+                  <p>{deliveryAddress?.country?.name}</p>
+                </div>
+              </div>
             </div>
           </div>
-          <Separator />
-          <div className="flex flex-col gap-4">
-            <h6 className="font-semibold">{i18n.get('Delivery address')}</h6>
-            <div className="flex flex-col gap-4">
-              <h6 className="font-semibold text-base">
-                {clientPartner?.fullName}, {company?.name}
-              </h6>
-              <div className="text-sm leading-[1.313rem]">
-                <p>{deliveryAddress?.addressl4}</p>
-                <p>{deliveryAddressCity}</p>
-                <p>{deliveryAddress?.zip}</p>
-                <p>{deliveryAddress?.country?.name}</p>
-              </div>
-            </div>
-            <div className="flex">
-              <Button className="bg-white border border-success text-success hover:bg-success hover:text-white rounded-md font-medium text-base py-1.5 px-3">
-                {i18n.get('Choose another address')}
-              </Button>
-            </div>
+          <div className="flex">
+            <Button
+              className="bg-white border border-success text-success hover:bg-success hover:text-white rounded-md font-medium text-base py-1.5 px-3"
+              onClick={onAddressSelection}>
+              {i18n.get('Choose another address')}
+            </Button>
           </div>
         </div>
       </div>
