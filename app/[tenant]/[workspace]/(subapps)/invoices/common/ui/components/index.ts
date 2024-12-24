0929587eddf024b8e1bc08b5974d922b2bc6705b
sga-axelor <115212874+sga-axelor@users.noreply.github.com>
2024-12-24 12:50:39 +0530

    refactor: remove unused "invoice-content" component from invoices

--- app/[tenant]/[workspace]/(subapps)/invoices/common/ui/components/index.ts
+++ app/[tenant]/[workspace]/(subapps)/invoices/common/ui/components/index.ts
@@ -1,5 +1,4 @@
 export {Invoice} from './invoice';
-export {InvoiceContent} from './invoice-content';
 export {InvoiceTable} from './invoice-table';
 export {Total} from './total';
 export {Columns, UnpaidColumns} from './columns';
