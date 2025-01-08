3398279b5e4eb2a2648967ab4e77dfb3460fcfef
kac-axelor <215158475+kac-axelor@users.noreply.github.com>
2025-01-08 17:46:32 +0530

    chore: remove unused function

--- app/[tenant]/[workspace]/(subapps)/directory/common/utils/index.ts
+++ app/[tenant]/[workspace]/(subapps)/directory/common/utils/index.ts
@@ -1,20 +1,8 @@
 import type {Maybe} from '@/types/util';
 import {defaultSortOption, sortOptions} from '../constants';
-import type {Tenant} from '@/lib/core/tenant';
-import {getImageURL} from '@/utils/files';
 
 export function getOrderBy(sort: Maybe<string>): Record<string, any> {
   return (sortOptions.find(o => o.value == sort) ?? defaultSortOption).orderBy;
-}
-
-export function getProfilePic(
-  id: string | undefined,
-  tenantId: Tenant['id'],
-): string {
-  return getImageURL(id, tenantId, {
-    noimage: true,
-    noimageSrc: '/images/user.png',
-  });
 }
 
 export const getPages = (
