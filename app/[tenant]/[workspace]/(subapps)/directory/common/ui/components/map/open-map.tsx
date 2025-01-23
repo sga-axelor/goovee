1e60ef74badfd49ff79c22649a4f94b96f353eb1
kac-axelor <215158475+kac-axelor@users.noreply.github.com>
2025-01-23 13:14:14 +0530

    refactor: remove react-leaflet and implement map with leaflet

--- app/[tenant]/[workspace]/(subapps)/directory/common/ui/components/map/open-map.tsx
+++ app/[tenant]/[workspace]/(subapps)/directory/common/ui/components/map/open-map.tsx
@@ -1,47 +1,56 @@
-import {MapContainer, Marker, Popup, TileLayer} from 'react-leaflet';
+'use client';
+import L from 'leaflet';
+import {memo, useLayoutEffect, useRef} from 'react';
+import ReactDOM from 'react-dom/client';
 
 import {useWorkspace} from '@/app/[tenant]/[workspace]/workspace-context';
 
 import {Card} from '../card';
-import type {MapContentProps} from './types';
+import {MapContentProps} from './types';
 
 import 'leaflet-defaulticon-compatibility';
 import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css'; // Re-uses images from ~leaflet package
 import 'leaflet/dist/leaflet.css';
 
-export function Map(props: MapContentProps) {
+export const Map = memo((props: MapContentProps) => {
+  const mapRef = useRef<HTMLDivElement>(null);
   const {className, center, zoom, items, small} = props;
   const {workspaceURI, tenant} = useWorkspace();
-  const key = className + center.lat + center.lng + zoom;
 
-  return (
-    <MapContainer
-      key={key}
-      className={className}
-      center={[center.lat, center.lng]}
-      zoom={zoom}
-      scrollWheelZoom={true}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      {items?.map(item => {
-        const url = `${workspaceURI}/directory/entry/${item.id}`;
-        return (
-          <Marker
-            key={item.id}
-            position={[
-              Number(item.address?.latit),
-              Number(item.address?.longit),
-            ]}>
-            <Popup
-              {...(!small && {minWidth: 500})}
-              className="[&_.leaflet-popup-content]:m-0">
-              <Card item={item} url={url} small={small} tenant={tenant} />
-            </Popup>
-          </Marker>
-        );
-      })}
-    </MapContainer>
-  );
-}
+  useLayoutEffect(() => {
+    if (!mapRef.current) return;
+    const map = new L.Map(mapRef.current, {
+      center: [center.lat, center.lng],
+      zoom,
+    });
+    // add tile layer with attribution. Without tile layer, the map will not be displayed
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    items?.forEach(item => {
+      const url = `${workspaceURI}/directory/entry/${item.id}`;
+      const popup = document.createElement('div');
+      ReactDOM.createRoot(popup).render(
+        <Card item={item} url={url} small={small} tenant={tenant} />,
+      );
+      L.marker([Number(item.address?.latit), Number(item.address?.longit)])
+        .addTo(map)
+        .bindPopup(popup, {
+          ...(!small && {minWidth: 500}),
+          className: '[&_.leaflet-popup-content]:m-0',
+        });
+    });
+
+    return () => {
+      map.off();
+      map.remove();
+    };
+    //NOTE: className is added to trigger a re-render if it affects the map's dimensions.
+  }, [zoom, center, items, workspaceURI, tenant, small, className]);
+
+  return <div ref={mapRef} className={className} />;
+});
+
+Map.displayName = 'Map';
