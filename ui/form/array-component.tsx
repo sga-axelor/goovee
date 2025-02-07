cf3256922928d4d914673ea8604c62982e571209
kac-axelor <215158475+kac-axelor@users.noreply.github.com>
2025-02-07 15:29:44 +0530

    feat: add participant sequence (#90656)

--- ui/form/array-component.tsx
+++ ui/form/array-component.tsx
@@ -7,6 +7,7 @@ import {MdAdd, MdOutlineDelete} from 'react-icons/md';
 import {Button} from '@/ui/components';
 import type {Field} from '@/ui/form';
 import {createDefaultValues, sortFields} from '@/ui/form';
+import {i18n} from '@/locale';
 
 export const ArrayComponent = ({
   form,
@@ -62,20 +63,23 @@ export const ArrayComponent = ({
   const renderArrayItem = useCallback(
     (subField: any, idx: any) => {
       return (
-        <div
-          key={subField.valueId}
-          className="p-4 flex gap-6 border rounded-lg">
-          <Button
-            onClick={() => removeItem(subField.valueId)}
-            type="button"
-            size="icon"
-            className={`bg-destructive-dark/20 hover:bg-destructive-dark/40 w-10 h-10 p-2`}>
-            <MdOutlineDelete className="w-6 h-6 text-destructive-dark" />
-          </Button>
-          <div className="space-y-6 w-full">
-            {visibleChildrenFields.map(_i =>
-              renderItem(_i, `${field.name}.${idx}.${_i.name}`),
-            )}
+        <div className="border rounded-lg p-4 ">
+          <h3 className="text-lg font-semibold leading-6 tracking-tight mb-2">
+            {i18n.t('Participant')} #{idx + 2}
+          </h3>
+          <div key={subField.valueId} className="flex gap-6 ">
+            <Button
+              onClick={() => removeItem(subField.valueId)}
+              type="button"
+              size="icon"
+              className={`bg-destructive-dark/20 hover:bg-destructive-dark/40 w-10 h-10 p-2`}>
+              <MdOutlineDelete className="w-6 h-6 text-destructive-dark" />
+            </Button>
+            <div className="space-y-6 w-full">
+              {visibleChildrenFields.map(_i =>
+                renderItem(_i, `${field.name}.${idx}.${_i.name}`),
+              )}
+            </div>
           </div>
         </div>
       );
