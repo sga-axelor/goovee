631970c9314c0665e97307d698fe7af3aef22deb
kac-axelor <215158475+kac-axelor@users.noreply.github.com>
2025-02-11 11:07:52 +0530

    fix: use template string for html (#90284)

--- app/[tenant]/[workspace]/(subapps)/events/common/utils/mail.ts
+++ app/[tenant]/[workspace]/(subapps)/events/common/utils/mail.ts
@@ -1,5 +1,6 @@
 // ---- CORE IMPORTS ---- //
 import {Participant} from '@/types';
+import {html} from '@/utils/template-string';
 
 export function mailTemplate({
   event,
@@ -21,77 +22,79 @@ export function mailTemplate({
   const fullName = `${name} ${surname}`.trim();
 
   const dateDetails = eventAllDay
-    ? `<strong>Date:</strong> ${formattedEventStartDateTime}`
-    : `<strong>Date:</strong> ${formattedEventStartDateTime} - ${formattedEventEndDateTime}`;
+    ? html`<strong>Date:</strong> ${formattedEventStartDateTime}`
+    : html`<strong>Date:</strong> ${formattedEventStartDateTime} -
+        ${formattedEventEndDateTime}`;
 
   const subscriptionDetails = subscriptionSet?.length
     ? subscriptionSet
-        .map((subscription: any) => `<li>${subscription.facility}</li>`)
+        .map((subscription: any) => html`<li>${subscription.facility}</li>`)
         .join('')
     : null;
 
-  return `
-    <!DOCTYPE html>
+  return html`
+    <!doctype html>
     <html>
-    <head>
-      <style>
-        body {
-          font-family: 'Arial', sans-serif;
-          background-color: #f9f9f9;
-          color: #333;
-          padding: 20px;
-        }
-        .container {
-          max-width: 600px;
-          margin: 0 auto;
-          background: #fff;
-          border-radius: 8px;
-          box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-          overflow: hidden;
-        }
-        .header {
-          background: #5603AD;
-          color: #fff;
-          padding: 20px;
-          text-align: center;
-        }
-        .content {
-          padding: 20px;
-          line-height: 1.6;
-        }
-        .facilities-title {
-          margin: 0;
-        }
-        .facility-list {
-          margin: 0;
-          padding-left: 20px;
-        }
-      </style>
-    </head>
-    <body>
-      <div class="container">
-        <div class="header">
-          <h1>Welcome to the Event!</h1>
-        </div>
-        <div class="content">
-          <p>Hi <b>${fullName}</b>,</p>
-          <p>Thank you for registering for our upcoming event. Here are the details:</p>
-          <p>
-            <strong>Event Name:</strong> ${eventTitle}<br>
-            ${dateDetails}<br>
-            ${eventPlace ? `<strong>Location:</strong> ${eventPlace}` : ''}
-          </p>
-          ${
-            subscriptionDetails
+      <head>
+        <style>
+          body {
+            font-family: 'Arial', sans-serif;
+            background-color: #f9f9f9;
+            color: #333;
+            padding: 20px;
+          }
+          .container {
+            max-width: 600px;
+            margin: 0 auto;
+            background: #fff;
+            border-radius: 8px;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+            overflow: hidden;
+          }
+          .header {
+            background: #5603ad;
+            color: #fff;
+            padding: 20px;
+            text-align: center;
+          }
+          .content {
+            padding: 20px;
+            line-height: 1.6;
+          }
+          .facilities-title {
+            margin: 0;
+          }
+          .facility-list {
+            margin: 0;
+            padding-left: 20px;
+          }
+        </style>
+      </head>
+      <body>
+        <div class="container">
+          <div class="header">
+            <h1>Welcome to the Event!</h1>
+          </div>
+          <div class="content">
+            <p>Hi <b>${fullName}</b>,</p>
+            <p>
+              Thank you for registering for our upcoming event. Here are the
+              details:
+            </p>
+            <p>
+              <strong>Event Name:</strong> ${eventTitle}<br />
+              ${dateDetails}<br />
+              ${eventPlace ? `<strong>Location:</strong> ${eventPlace}` : ''}
+            </p>
+            ${subscriptionDetails
               ? `<p class="facilities-title"><strong>Facilities:</strong></p>
                   <ul class="facility-list">${subscriptionDetails}</ul>`
-              : ''
-          }
-          ${eventDescription ? `<p>${eventDescription}</p>` : ''}
-          <p>We look forward to seeing you there!</p>
+              : ''}
+            ${eventDescription ? `<p>${eventDescription}</p>` : ''}
+            <p>We look forward to seeing you there!</p>
+          </div>
         </div>
-      </div>
-    </body>
+      </body>
     </html>
   `;
 }
