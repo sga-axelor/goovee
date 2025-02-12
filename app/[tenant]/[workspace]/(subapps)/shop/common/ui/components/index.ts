d0369efb2ad7f245b685b445019d77d68924e3d6
kk-axelor <55643822+kk-axelor@users.noreply.github.com>
2025-02-12 11:42:31 +0530

    fix: remove unused component from shop app (#90085)

--- app/[tenant]/[workspace]/(subapps)/shop/common/ui/components/index.ts
+++ app/[tenant]/[workspace]/(subapps)/shop/common/ui/components/index.ts
@@ -5,7 +5,6 @@ export {ProductView} from './product-view';
 export {ProductList} from './product-list';
 export {ProductCard} from './product-card';
 export {ProductListItem} from './product-list-item';
-export {Categories} from './categories';
 export {ProductListColorFilter} from './product-list-color-filter';
 export {ProductListBrandFilter} from './product-list-brand-filter';
 export {FeaturedCategories} from './featured-categories';
