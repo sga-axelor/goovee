8adfac72fb712f8d4bc199c5c50d2cd048d64560
kac-axelor <215158475+kac-axelor@users.noreply.github.com>
2025-02-18 17:31:42 +0530

    refactor: move create invoice to utils

--- app/[tenant]/[workspace]/(subapps)/events/common/actions/payments.ts
+++ app/[tenant]/[workspace]/(subapps)/events/common/actions/payments.ts
@@ -1,6 +1,5 @@
 'use server';
 
-import axios from 'axios';
 import {headers} from 'next/headers';
 
 // ---- CORE IMPORTS ---- //
@@ -12,8 +11,7 @@ import {findPartnerByEmail} from '@/orm/partner';
 import {findSubappAccess, findWorkspace} from '@/orm/workspace';
 import {createPaypalOrder} from '@/payment/paypal/actions';
 import {createStripeOrder, findStripeOrder} from '@/payment/stripe/actions';
-import {manager, type Tenant} from '@/tenant';
-import {ID, PaymentOption} from '@/types';
+import {PaymentOption} from '@/types';
 import {isPaymentOptionAvailable} from '@/utils/payment';
 import {formatAmountForStripe} from '@/utils/stripe';
 
@@ -21,97 +19,10 @@ import {formatAmountForStripe} from '@/utils/stripe';
 import {register} from '@/subapps/events/common/actions/actions';
 import {REQUIRED_FIELDS} from '@/subapps/events/common/constants';
 import {findEvent} from '@/subapps/events/common/orm/event';
-import {findEventRegistration} from '@/subapps/events/common/orm/registration';
 import {error} from '@/subapps/events/common/utils';
+import {createInvoice} from '@/subapps/events/common/utils/invoice';
 import {getCalculatedTotalPrice} from '@/subapps/events/common/utils/payments';
 import {validateRequiredFormFields} from '@/subapps/events/common/utils/registration';
-
-export async function createInvoice({
-  workspaceURL,
-  tenantId,
-  registrationId,
-  eventId,
-}: {
-  workspaceURL: string;
-  tenantId: Tenant['id'];
-  registrationId: ID;
-  eventId: ID;
-}) {
-  if (!registrationId) {
-    return error(await t('Event registration ID is required.'));
-  }
-
-  const tenant = await manager.getTenant(tenantId);
-  const aos = tenant?.config?.aos;
-
-  if (!aos?.url) {
-    return error(await t('Invoice creation failed. Webservice not available.'));
-  }
-
-  const ws = `${aos.url}/ws/portal/invoice/eventInvoice`;
-
-  try {
-    const session = await getSession();
-    const user = session?.user;
-
-    const workspace = await findWorkspace({url: workspaceURL, user, tenantId});
-    if (!workspace) {
-      return error(await t('Invalid workspace.'));
-    }
-
-    const partnerWorkspaceId = workspace?.workspacePermissionConfig?.id;
-    if (!partnerWorkspaceId) {
-      return error(await t('Partner workspace id is missing.'));
-    }
-
-    const event = await findEvent({id: eventId, workspace, tenantId, user});
-    if (!event) {
-      return error(await t('Event not found.'));
-    }
-
-    const eventRegistration = await findEventRegistration({
-      workspaceURL,
-      tenantId,
-      id: registrationId,
-      eventId: event.id,
-    });
-
-    if (!eventRegistration) {
-      return error(await t('Event registration not found.'));
-    }
-
-    const payload = {
-      currencyCode: event.currency.code,
-      partnerWorkspaceId,
-      registrationId: eventRegistration.id,
-    };
-
-    const {data} = await axios.post(ws, payload, {
-      auth: {
-        username: aos.auth.username,
-        password: aos.auth.password,
-      },
-    });
-
-    if (data?.status === -1) {
-      return error(
-        await t(
-          data?.message ||
-            'Unable to create the invoice. Please try again later.',
-        ),
-      );
-    }
-
-    return data;
-  } catch (err) {
-    console.error('Invoice creation failed:', err);
-    return error(
-      await t(
-        'An error occurred while processing your invoice. Please try again later.',
-      ),
-    );
-  }
-}
 
 export async function createStripeCheckoutSession({
   event,
---
+++ app/[tenant]/[workspace]/(subapps)/events/common/utils/invoice.ts
@@ -0,0 +1,100 @@
+import axios from 'axios';
+
+// ---- CORE IMPORTS ---- //
+import {getSession} from '@/auth';
+import {t} from '@/locale/server';
+import {findWorkspace} from '@/orm/workspace';
+import {manager, type Tenant} from '@/tenant';
+import {ID} from '@/types';
+
+// ---- LOCAL IMPORTS ---- //
+import {findEvent} from '@/subapps/events/common/orm/event';
+import {findEventRegistration} from '@/subapps/events/common/orm/registration';
+import {error} from '@/subapps/events/common/utils';
+
+export async function createInvoice({
+  workspaceURL,
+  tenantId,
+  registrationId,
+  eventId,
+}: {
+  workspaceURL: string;
+  tenantId: Tenant['id'];
+  registrationId: ID;
+  eventId: ID;
+}) {
+  if (!registrationId) {
+    return error(await t('Event registration ID is required.'));
+  }
+
+  const tenant = await manager.getTenant(tenantId);
+  const aos = tenant?.config?.aos;
+
+  if (!aos?.url) {
+    return error(await t('Invoice creation failed. Webservice not available.'));
+  }
+
+  const ws = `${aos.url}/ws/portal/invoice/eventInvoice`;
+
+  try {
+    const session = await getSession();
+    const user = session?.user;
+
+    const workspace = await findWorkspace({url: workspaceURL, user, tenantId});
+    if (!workspace) {
+      return error(await t('Invalid workspace.'));
+    }
+
+    const partnerWorkspaceId = workspace?.workspacePermissionConfig?.id;
+    if (!partnerWorkspaceId) {
+      return error(await t('Partner workspace id is missing.'));
+    }
+
+    const event = await findEvent({id: eventId, workspace, tenantId, user});
+    if (!event) {
+      return error(await t('Event not found.'));
+    }
+
+    const eventRegistration = await findEventRegistration({
+      workspaceURL,
+      tenantId,
+      id: registrationId,
+      eventId: event.id,
+    });
+
+    if (!eventRegistration) {
+      return error(await t('Event registration not found.'));
+    }
+
+    const payload = {
+      currencyCode: event.currency.code,
+      partnerWorkspaceId,
+      registrationId: eventRegistration.id,
+    };
+
+    const {data} = await axios.post(ws, payload, {
+      auth: {
+        username: aos.auth.username,
+        password: aos.auth.password,
+      },
+    });
+
+    if (data?.status === -1) {
+      return error(
+        await t(
+          data?.message ||
+            'Unable to create the invoice. Please try again later.',
+        ),
+      );
+    }
+
+    return data;
+  } catch (err) {
+    console.error('Invoice creation failed:', err);
+    return error(
+      await t(
+        'An error occurred while processing your invoice. Please try again later.',
+      ),
+    );
+  }
+}
