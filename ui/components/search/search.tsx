9e4753f3521c0ccb89d839d7eab45a1d3cfb68c5
kk-axelor <55643822+kk-axelor@users.noreply.github.com>
2025-02-26 16:06:22 +0530

    fix: set loading state in event search (#91537)

--- ui/components/search/search.tsx
+++ ui/components/search/search.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, {useCallback, useEffect, useState} from 'react';
+import React, {useCallback, useEffect, useState, useRef} from 'react';
 import {debounce} from 'lodash';
 
 // ---- CORE IMPORTS ---- //
@@ -13,7 +13,6 @@ import {
   CommandItem,
   CommandList,
 } from '@/ui/components/command';
-import {NO_RESULTS_FOUND} from '@/constants';
 import {i18n} from '@/locale';
 
 export const Search = ({
@@ -41,14 +40,24 @@ export const Search = ({
   const [search, setSearch] = useState('');
   const [results, setResults] = useState<any[]>([]);
   const [open, setOpen] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
+  const searchRef = useRef<string | undefined>();
 
   const debouncedFindQuery = useCallback(
     debounce(async (query: string) => {
-      if (query) {
-        const results = await findQuery({query});
-        setResults(results);
-      } else {
-        setResults([]);
+      try {
+        if (query) {
+          const results = await findQuery({query});
+          setResults(results);
+        } else {
+          setResults([]);
+        }
+      } catch (error) {
+        console.error('Error fetching search results:', error);
+      } finally {
+        if (searchRef.current === search) {
+          setLoading(false);
+        }
       }
     }, 500),
     [findQuery],
@@ -73,9 +82,11 @@ export const Search = ({
             placeholder={i18n.t('Search here')}
             className="lg:placeholder:text-base placeholder:text-sm placeholder:font-normal lg:placeholder:font-medium pl-[10px] py-4 pr-[132px] h-14 lg:pl-4 border-none text-base font-medium rounded-lg focus-visible:ring-offset-0 focus-visible:ring-0 text-main-black"
             value={search}
-            onChangeCapture={(e: React.ChangeEvent<HTMLInputElement>) =>
-              setSearch(e.target.value)
-            }
+            onChangeCapture={(e: React.ChangeEvent<HTMLInputElement>) => {
+              setSearch(e.target.value);
+              setLoading(true);
+              searchRef.current = e.target.value;
+            }}
             onFocus={() => onFocus && onFocus(true)}
             onKeyDown={e => onKeyDown && onKeyDown(e, search)}
           />
@@ -86,7 +97,9 @@ export const Search = ({
               open ? 'block' : 'hidden',
               forceClose ? 'hidden' : '',
             )}>
-            <CommandEmpty>{NO_RESULTS_FOUND}</CommandEmpty>
+            <CommandEmpty>
+              {loading ? i18n.t('Searching...') : i18n.t('No results found.')}
+            </CommandEmpty>
             <CommandGroup className="p-2">
               {Boolean(results?.length)
                 ? results.map((result: any) => (
