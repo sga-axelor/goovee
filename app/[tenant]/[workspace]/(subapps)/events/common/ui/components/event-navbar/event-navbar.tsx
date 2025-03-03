d811e1533b6cd23bed1893f6719b4f18b4666a42
kk-axelor <55643822+kk-axelor@users.noreply.github.com>
2025-03-03 15:51:45 +0530

    fix: hide event navbar for small screen (#91596)

--- app/[tenant]/[workspace]/(subapps)/events/common/ui/components/event-navbar/event-navbar.tsx
+++ app/[tenant]/[workspace]/(subapps)/events/common/ui/components/event-navbar/event-navbar.tsx
@@ -1,13 +1,13 @@
 'use client';
 import React from 'react';
-import {useRouter} from 'next/navigation';
 import Link from 'next/link';
 
 // ---- CORE IMPORTS ---- //
 import {Separator} from '@/ui/components/separator';
-import {SUBAPP_CODES} from '@/constants';
+import {RESPONSIVE_SIZES, SUBAPP_CODES} from '@/constants';
 import {User} from '@/types';
 import {i18n} from '@/locale';
+import {useResponsive} from '@/ui/hooks';
 
 // ---- LOCAL IMPORTS ---- //
 import {EVENTS_NAVBAR_LINKS} from '@/subapps/events/common/constants';
@@ -21,30 +21,37 @@ type nav_link = {
 };
 
 export const EventNavbar = ({user}: {user?: User}) => {
-  const router = useRouter();
   const {workspaceURI} = useWorkspace();
-
+  const res = useResponsive();
+  const small = RESPONSIVE_SIZES.some(x => res[x]);
   const filteredLinks: nav_link[] = EVENTS_NAVBAR_LINKS.filter(
     link => !(link.validate && !user?.email),
   );
 
   return (
-    <div className="py-4 px-6 bg-white">
-      <div className="h-8 flex gap-10 overflow-x-auto items-center justify-end">
-        {filteredLinks.map((item: nav_link, i: number) => (
-          <React.Fragment key={item.id}>
-            <Link
-              href={`${workspaceURI}/${SUBAPP_CODES.events}${item.redirectTo}`}>
-              <div className="font-medium text-base cursor-pointer">
-                {i18n.t(item.title)}
-              </div>
-            </Link>
-            {i !== filteredLinks.length - 1 && (
-              <Separator className="bg-black w-[2px]" orientation="vertical" />
-            )}
-          </React.Fragment>
-        ))}
-      </div>
+    <div>
+      {!small && (
+        <div className="py-4 px-6 bg-white ">
+          <div className="h-8 flex gap-10 overflow-x-auto items-center justify-end">
+            {filteredLinks.map((item: nav_link, i: number) => (
+              <React.Fragment key={item.id}>
+                <Link
+                  href={`${workspaceURI}/${SUBAPP_CODES.events}${item.redirectTo}`}>
+                  <div className="font-medium text-base cursor-pointer">
+                    {i18n.t(item.title)}
+                  </div>
+                </Link>
+                {i !== filteredLinks.length - 1 && (
+                  <Separator
+                    className="bg-black w-[2px]"
+                    orientation="vertical"
+                  />
+                )}
+              </React.Fragment>
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
