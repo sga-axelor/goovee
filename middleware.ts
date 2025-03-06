858075eeef293aad766ef2fecf64ca9600cf99b2
npa-axelor <92723622+npa-axelor@users.noreply.github.com>
2025-03-06 21:17:14 +0530

    chore: fix lint

--- middleware.ts
+++ middleware.ts
@@ -67,7 +67,7 @@ export default async function middleware(req: NextRequest) {
   }
 
   const headers = new Headers(req.headers);
-  
+
   if (tenant) {
     headers.set(TENANT_HEADER, tenant);
   }
