b052210da86eeb65891018db3ab251cfd43e4b22
npa-axelor <92723622+npa-axelor@users.noreply.github.com>
2025-04-07 15:25:20 +0530

    fix: remove email sync between contact and partner

--- app/[tenant]/[workspace]/account/personal/action.ts
+++ app/[tenant]/[workspace]/account/personal/action.ts
@@ -276,34 +276,6 @@ export async function update({
       };
 
       await updateEmail(partner?.emailAddress, email);
-
-      /**
-       * Check if the email belongs to contact, if so then update
-       * contact email along with this update. This is in respect to
-       * registration flow where a contact can register partner (#92494)
-       */
-
-      if (!partner.isContact) {
-        const contact = await client.aOSPartner.findOne({
-          where: {
-            mainPartner: {id: partner.id},
-            isContact: {eq: true},
-            emailAddress: {
-              address: partner.emailAddress.address,
-            },
-            isActivatedOnPortal: {
-              ne: true,
-            },
-          },
-          select: {
-            emailAddress: true,
-          },
-        });
-
-        if (contact?.emailAddress) {
-          await updateEmail(contact?.emailAddress, email);
-        }
-      }
     }
 
     const updatedPartner = await updatePartner({
