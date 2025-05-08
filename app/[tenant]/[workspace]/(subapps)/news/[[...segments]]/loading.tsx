a66b114e124a38cf48e76fd94c602a31731c7012
sga-axelor <115212874+sga-axelor@users.noreply.github.com>
2025-05-08 15:51:51 +0530

    feat: add loading component for news app (#94767)

---
+++ app/[tenant]/[workspace]/(subapps)/news/[[...segments]]/loading.tsx
@@ -0,0 +1,12 @@
+import {Skeleton} from '@/ui/components';
+import styles from '@/subapps/news/common/ui/styles/news.module.scss';
+
+export default function Loading() {
+  return (
+    <div className={`grid grid-cols-1 p-4  h-full ${styles['news-container']}`}>
+      <Skeleton className="flex items-center justify-center !border-0">
+        <div className="w-6 h-6 border-2 border-t-transparent border-gray-400 rounded-full animate-spin" />
+      </Skeleton>
+    </div>
+  );
+}
