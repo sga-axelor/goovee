ab525ff11b75c37ce265624b26e5b4d6152d393a
npa-axelor <92723622+npa-axelor@users.noreply.github.com>
2025-05-28 14:40:03 +0530

    style: fix attachment list styling (#95793)

--- app/[tenant]/[workspace]/(subapps)/news/common/ui/components/attachment-list/attachment-list.tsx
+++ app/[tenant]/[workspace]/(subapps)/news/common/ui/components/attachment-list/attachment-list.tsx
@@ -45,21 +45,26 @@ export const AttachmentList = ({
       <div className="font-semibold text-xl mb-4">{title}</div>
       <Separator />
       <div className="mt-4">
-        {items?.map((item, i) => (
-          <div
-            key={item.id}
-            className="mb-4 grid grid-cols-[4fr_2fr_1fr] gap-2 shrink-0 items-center">
-            <p className="text-[16px] font-medium">
-              {item?.title || item?.metaFile?.fileName}
-            </p>
-            <div className="text-end">{item?.metaFile?.sizeText}</div>
+        {items?.map((item, i) => {
+          const label = item?.title || item?.metaFile?.fileName;
+          return (
             <div
-              className="flex justify-end"
-              onClick={() => handleDownload(item)}>
-              <MdOutlineFileDownload className="text-2xl font-semibold cursor-pointer" />
+              key={item.id}
+              className="mb-4 grid grid-cols-[4fr_2fr_1fr] gap-2 shrink-0 items-center">
+              <p
+                className="text-[16px] font-medium overflow-hidden"
+                title={label}>
+                {label}
+              </p>
+              <div className="text-end">{item?.metaFile?.sizeText}</div>
+              <div
+                className="flex justify-end"
+                onClick={() => handleDownload(item)}>
+                <MdOutlineFileDownload className="text-2xl font-semibold cursor-pointer" />
+              </div>
             </div>
-          </div>
-        ))}
+          );
+        })}
       </div>
     </div>
   );
