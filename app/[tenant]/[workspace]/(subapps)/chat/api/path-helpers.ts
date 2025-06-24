c3ec77a74c2388b5a61f43d521486450a8afbd2d
npa-axelor <92723622+npa-axelor@users.noreply.github.com>
2025-06-24 15:35:04 +0530

    chore: remove dead code

--- app/[tenant]/[workspace]/(subapps)/chat/api/path-helpers.ts
+++ app/[tenant]/[workspace]/(subapps)/chat/api/path-helpers.ts
@@ -6,23 +6,3 @@ export const CHANNELS_API_ENDPOINT = `${PATH_ROOT}/channels`;
 export const POSTS_API_ENDPOINT = `${PATH_ROOT}/posts`;
 export const REACTIONS_API_ENDPOINT = `${PATH_ROOT}/reactions`;
 export const FILES_API_ENDPOINT = `${PATH_ROOT}/files`;
-
-// /**
-//  * Remove all the special char (minus space and - )
-//  * Replace all the space by -
-//  * Remove all accents
-//  * Make it lowerCase
-//  * @param str the string to format
-//  */
-// export const formatNameForApi = (str: string) => {
-//   if (!str) {
-//     return '';
-//   }
-
-//   return str
-//     .replaceAll(' ', '-')
-//     .replaceAll(/[~`!@#$%^&*(){}\[\];:"'<,.>?\/\\|_+=]/g, '')
-//     .normalize('NFD')
-//     .replaceAll(/[\u0300-\u036f]/g, '')
-//     .toLowerCase();
-// };
