4c0ab7d3483d38e4ed2649f1570d5874bd7e86bf
npa-axelor <92723622+npa-axelor@users.noreply.github.com>
2025-07-02 16:16:59 +0530

    fix: remove survey application (#97461)
    - Move survey application, schema and configuration to goovee enterprise repository

--- app/[tenant]/[workspace]/(subapps)/survey/common/ui/components/index.ts
+++
@@ -1,8 +0,0 @@
-export {Chip} from './chip';
-export {
-  numberResponsesColumn,
-  partnerResponseColumns,
-  surveyColumns,
-} from './columns';
-export {SearchItem} from './search-item';
-export {SurveyViewer} from './survey-viewer';
--- app/[tenant]/[workspace]/(subapps)/survey/common/utils/index.ts
+++
@@ -1,5 +0,0 @@
-import {STATUS_NAMES} from '@/subapps/survey/common/constants';
-
-export const getStatusName = (value: number): string => {
-  return STATUS_NAMES[value];
-};
