e7fb18f49c951bd58c786b907d212a928208c84f
Nishant Painter <n.painter@axelor.com>
2025-09-30 11:55:59 +0530

    fix: prompt credentials on subsequent login (#100832)

--- lib/core/auth/(ee)/keycloak.ts
+++ lib/core/auth/(ee)/keycloak.ts
@@ -4,6 +4,11 @@ export const keycloak = Keycloak({
   clientId: process.env.KEYCLOAK_ID as string,
   clientSecret: process.env.KEYCLOAK_SECRET as string,
   issuer: process.env.KEYCLOAK_ISSUER as string,
+  authorization: {
+    params: {
+      prompt: 'login',
+    },
+  },
 });
 
 export default keycloak;
