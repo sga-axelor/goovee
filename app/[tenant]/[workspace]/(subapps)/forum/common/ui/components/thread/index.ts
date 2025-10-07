e375c7f4c75af2aa0ec44d79f06a4542b949053d
s.gaur <115212874+sga-axelor@users.noreply.github.com>
2025-10-07 11:08:28 +0530

    chore(format): remove extra space in ThreadHeader (#96451)

--- app/[tenant]/[workspace]/(subapps)/forum/common/ui/components/thread/index.ts
+++ app/[tenant]/[workspace]/(subapps)/forum/common/ui/components/thread/index.ts
@@ -1,4 +1,4 @@
 export * from './thread';
-export {ThreadHeader } from './thread-header';
+export {ThreadHeader} from './thread-header';
 export {ThreadBody} from './thread-body';
 export {ThreadFooter} from './thread-footer';
